dc78f8182ce48a97b38c0221da814b682d7ca3f3
Cantidio Fontes <aniquilatorbloody@gmail.com>
2016-03-26 20:36:53 -0700

    github-pages publish.

--- bin/github-pages.js
+++ bin/github-pages.js
@@ -5,14 +5,17 @@ var meow = require('meow');
 var parseConfig = require('../lib/parse-config');
 var GithubPages = require('../lib');
 var cli = meow({
-  help: '\n  Usage\n    $ github-pages [options] [src]\n\n    Options\n    -r, --repository\n    -t, --token\n        --api-version\n        --api-protocol\n        --api-host\n        --api-path\n        --api-timeout\n\n  Examples\n    $ github-pages -r cantidio/github-pages -t $GH_TOKEN ./data\n      > github-pages commit\n      > github-pages push to cantidio/github-pages\n' }, {
+  help: '\n  Usage\n    $ github-pages [options] [src]\n\n    Options\n    -r, --repository\n    -t, --token\n    -m  --commit-message\n    -a  --commit-author\n        --remote-ref\n        --api-version\n        --api-protocol\n        --api-host\n        --api-path\n        --api-timeout\n\n  Examples\n    $ github-pages -r cantidio/github-pages -t $GH_TOKEN ./data\n      > github-pages commit\n      > github-pages push to cantidio/github-pages\n' }, {
   alias: {
     r: 'repo',
-    t: 'token'
+    t: 'token',
+    m: 'commit-message',
+    a: 'commit-author'
   }
 });
 
 var cfg = parseConfig(cli.flags, cli.input[0]);
+
 if (!cfg) {
   console.log('provide the user, repo and token.');
   console.log(cli.help);
