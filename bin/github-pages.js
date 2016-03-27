e043a8f6b8f1540c7fc2d690fab239bf9edb269d
Cantidio Fontes <aniquilatorbloody@gmail.com>
2016-03-26 23:44:47 -0700

    custom message

--- bin/github-pages.js
+++ bin/github-pages.js
@@ -4,8 +4,9 @@
 var meow = require('meow');
 var parseConfig = require('../lib/parse-config');
 var GithubPages = require('../lib');
+
 var cli = meow({
-  help: '\n  Usage\n    $ github-pages [options] [src]\n\n    Options\n    -r, --repository\n    -t, --token\n    -m  --commit-message\n    -a  --commit-author\n        --remote-ref\n        --api-version\n        --api-protocol\n        --api-host\n        --api-path\n        --api-timeout\n\n  Examples\n    $ github-pages -r cantidio/github-pages -t $GH_TOKEN ./data\n      > github-pages commit\n      > github-pages push to cantidio/github-pages\n' }, {
+  help: '\n  Usage\n    $ github-pages [options] [src]\n\n    Options\n    -r, --repository\n    -t, --token\n    -m  --commit-message\n    -a  --commit-author\n        --remote-ref\n        --api-version\n        --api-protocol\n        --api-host\n        --api-path\n        --api-timeout\n\n  Examples\n    $ github-pages -r cantidio/node-github-pages -t $GH_TOKEN ./data\n      > github-pages commit\n      > github-pages push to cantidio/node-github-pages\n' }, {
   alias: {
     r: 'repo',
     t: 'token',
@@ -14,13 +15,21 @@ var cli = meow({
   }
 });
 
-var cfg = parseConfig(cli.flags, cli.input[0]);
+var config = parseConfig(cli.flags, cli.input[0]);
 
-if (!cfg) {
+if (!config) {
   console.log('provide the user, repo and token.');
   console.log(cli.help);
   process.exit(1);
 }
 
-var ghpages = new GithubPages(cfg);
-ghpages.run();
+var ghpages = new GithubPages(config);
+
+console.log('GithubPages:run.');
+console.log('GithubPages:publish ' + config.remote.ref);
+ghpages.publish().then(function (res) {
+  console.log('GithubPages:done');
+}).catch(function (err) {
+  console.error('GithubPages:error updating remote ref.');
+  process.exit(1);
+});
